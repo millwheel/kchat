c89c74cc90b32c1ab6a8899b1a1311003edc0a6b
박시준 <sjpark@logblack.com>
2024-04-14 20:46:48 +0900

    feat: typeorm의 repository 적용

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './entity/users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [];
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
-  getAll(): User[] {
-    return this.users;
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
   }
 }
