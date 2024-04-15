d25c65d4357d00dc4c1a337bb1d306ed483e4671
박시준 <sjpark@logblack.com>
2024-04-15 13:36:42 +0900

    feat: Add http status code decoration

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './entity/users.entity';
 import { DeleteResult } from 'typeorm';
@@ -8,21 +17,25 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   async getUsers(): Promise<User[]> {
     return await this.usersService.findAll();
   }
 
   @Get('/:username')
+  @HttpCode(HttpStatus.OK)
   async getUser(@Param('username') username: string): Promise<User> {
     return await this.usersService.findOne(username);
   }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   async createUser(@Body() user: User): Promise<User> {
     return await this.usersService.save(user);
   }
 
   @Delete(':username')
+  @HttpCode(HttpStatus.OK)
   async deleteUser(@Param('username') username: string): Promise<DeleteResult> {
     return this.usersService.delete(username);
   }
